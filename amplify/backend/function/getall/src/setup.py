cf689708d15132c8e1479ac8936e34d46b99dd08
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-08-09 11:27:03 -0300

    Revert "Add Functions of projects"
    This reverts commit 60f50155d05cdafb49c1a081f59472d80bed37c6.

--- amplify/backend/function/getall/src/setup.py
+++
@@ -1,3 +0,0 @@
-from distutils.core import setup
-import boto3
-setup(name='src', version='1.0')
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "oldbarom": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
   "auth": {
     "oldbarome209d2b3": {
       "AppClientID": "string",
@@ -18,36 +11,6 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "userPoolGroups": {
       "OldBaromUsersGroupRole": "string"
-    }
-  },
-  "function": {
-    "AddFavorite": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "RemoveFavorite": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "getall": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "getfavorites": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
     }
   }
 }
